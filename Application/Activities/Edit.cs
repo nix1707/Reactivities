c43ec2d797b0fba2821a9e953ee6e7c7b45a4e33
nix1707 <121435639+nix1707@users.noreply.github.com>
2024-04-27 15:48:24 +0300

    added CRUD operations for "Activity" model

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities;
+
+public class Edit
+{
+    public class Command : IRequest
+    {
+        public Activity Activity { get; set; }
+    }
+
+    public class Handler(DataContext context, IMapper mapper) : IRequestHandler<Command>
+    {
+        private readonly DataContext _context = context;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await _context.Activities.FindAsync(request.Activity.Id);
+            _mapper.Map(request.Activity, activity);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
