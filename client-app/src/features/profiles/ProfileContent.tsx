a8ad4dcce6c254e244d790d30bc67897802de887
nix1707 <121435639+nix1707@users.noreply.github.com>
2024-07-04 16:56:34 +0300

    introduced client image upload

---
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -0,0 +1,28 @@
+import { Tab, TabPane } from "semantic-ui-react"
+import ProfilePhotos from "./ProfilePhotos"
+import { Profile } from "../../app/models/profile"
+import { observer } from "mobx-react-lite";
+
+interface Props {
+    profile: Profile;
+}
+
+const ProfileContent = ({ profile }: Props) => {
+    const panes = [
+        { menuItem: 'About', render: () => <TabPane>About Content</TabPane> },
+        { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} /> },
+        { menuItem: 'Events', render: () => <TabPane>Events Content</TabPane> },
+        { menuItem: 'Followers', render: () => <TabPane>Followers Content</TabPane> },
+        { menuItem: 'Following', render: () => <TabPane>Following Content</TabPane> },
+    ]
+
+    return (
+        <Tab
+            menu={{ fluid: true, vertical: true }}
+            menuPosition="right"
+            panes={panes}
+        />
+    )
+}
+
+export default observer(ProfileContent)
